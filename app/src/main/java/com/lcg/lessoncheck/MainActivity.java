e9521b7f5d065fc2a16ffee61435a7597c536364
Mr.Li <lijy9811@qq.com>
2019-11-30 22:38:00 +0800

    navigation_menu

--- app/src/main/java/com/lcg/lessoncheck/MainActivity.java
+++ app/src/main/java/com/lcg/lessoncheck/MainActivity.java
@@ -3,12 +3,20 @@ package com.lcg.lessoncheck;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.nav_view);
+        LayoutInflater.from(MainActivity.this).inflate(R.layout.bottom_menu, bottomNavigationView, true);
+        bottomNavigationView.setVisibility(View.VISIBLE);
     }
 }
