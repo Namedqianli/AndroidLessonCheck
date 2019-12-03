1e7e30c69a3b99100830b0cdd728110332d2f575
Mr.Li <lijy9811@qq.com>
2019-12-04 05:11:01 +0800

    finish ui

--- app/src/main/java/com/lcg/lessoncheck/MainActivity.java
+++ app/src/main/java/com/lcg/lessoncheck/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private BottomNavigationView bottomNavigationView;
 
     @Override
@@ -18,5 +18,17 @@ public class MainActivity extends AppCompatActivity {
         bottomNavigationView = findViewById(R.id.nav_view);
         LayoutInflater.from(MainActivity.this).inflate(R.layout.bottom_menu, bottomNavigationView, true);
         bottomNavigationView.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.radius_image_view_home:
+                break;
+            case R.id.radius_image_view_message:
+                break;
+            case R.id.radius_image_view_info:
+                break;
+        }
     }
 }
